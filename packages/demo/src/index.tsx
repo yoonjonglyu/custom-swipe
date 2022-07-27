ab07f4697730aa1a12fa07a924b7108111883b8e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-07-27 15:59:55 +0900

    feat(ENTRY): BIND APP

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './App';
+
 const root = document.querySelector('#app');
 if (root !== null) {
-  createRoot(root).render(<div>init</div>);
+  createRoot(root).render(<App />);
 } else console.error('not find root container #app.');
