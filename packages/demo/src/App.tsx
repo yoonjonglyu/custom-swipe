bdd3e242b7d8f95a62cde6b483dc433b1a63a762
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-07-29 18:23:41 +0900

    feat(DEMO): 샘플 이미지 추가

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -2,11 +2,42 @@ import React from 'react';
 
 import Swipe from '../../react-simple-swipe/src';
 
+import cat1 from './image/cat1.jpg';
+import cat2 from './image/cat2.jpg';
+import cat3 from './image/cat3.jpg';
+import cat4 from './image/cat4.jpg';
+import cat5 from './image/cat5.jpeg';
+import cat6 from './image/cat6.jpeg';
+import cat7 from './image/cat7.jpg';
+import cat8 from './image/cat8.jpg';
+import cat9 from './image/cat9.png';
+import cat10 from './image/cat10.jpg';
+import iu1 from './image/iu1.jpg';
+import iu2 from './image/iu2.jpg';
+
 const App: React.FC = () => {
   return (
     <div>
       <h1>샘플 데모</h1>
-      <Swipe item={[1, 2, 3, 4, 5]} />
+      <Swipe
+        item={[
+          cat1,
+          cat2,
+          cat3,
+          cat4,
+          cat5,
+          cat6,
+          cat7,
+          cat8,
+          cat9,
+          cat10,
+          iu1,
+          iu2,
+        ].map((src, key) => (
+          <img key={key} src={src} alt={src} />
+        ))}
+        itemProps={{ style: { border: '1px solid', minHeight: '360px' } }}
+      />
     </div>
   );
 };
