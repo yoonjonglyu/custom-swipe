4f2d28b5727ce470a54639498ac51cce2ee30903
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-08-03 16:23:03 +0900

    fix(SWIPE): URI - 타입수정

--- packages/react-custom-swipe/src/lib/uri.ts
+++ packages/react-custom-swipe/src/lib/uri.ts
@@ -2,23 +2,20 @@ export function getURL() {
   return `${location.origin}${location.pathname}`;
 }
 export function getSearchParams() {
-  const result: { [key: string]: number | string } = {};
+  const result: { [key: string]: string } = {};
   const URI = new URLSearchParams(location.search);
   for (const [key, value] of URI.entries()) result[key] = value;
 
   return result;
 }
-export function formatQueryString(obj: { [key: string]: number | string }) {
+export function formatQueryString(obj: { [key: string]: string }) {
   let result = '?';
   const params = Object.entries(obj);
   for (const [key, value] of params) result += `${key}=${value}&`;
 
   return params.length > 0 ? result.slice(0, result.length - 1) : result;
 }
-export function setHistory(
-  data: { [key: string]: number | string },
-  url?: string,
-) {
+export function setHistory(data: { [key: string]: string }, url?: string) {
   history.pushState(
     data,
     '',
