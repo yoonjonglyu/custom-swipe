31f880fa865fed8afe21d29dd2e064593b2fc5b0
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-09-25 23:18:33 +0900

    refactor(CORE): SWIPE STATE 처리 로직 분리

--- packages/react-custom-swipe/src/lib/events.tsx
+++ packages/react-custom-swipe/src/lib/events.tsx
@@ -13,37 +13,20 @@ export default function SwipeEvents(
   itemLength: number,
   config?: ConfigProps,
 ) {
-  const swipeState = new SwipeState();
+  const swipeState = new SwipeState(itemLength);
   const index = config?.paramName ? config.paramName : 'index';
-  /**
-   * @description 스와이프 기능(플립액션)과 리사이즈 관련 된 로직들
-   */
+
   const handleStart = (e: Partial<TouchEvent & MouseEvent>) => {
-    if (swipeState.isSwipe === false) {
-      const x =
-        e.type === 'touchstart' && e.targetTouches
-          ? e.targetTouches[0].pageX
-          : e.pageX || 0;
-      const y =
-        e.type === 'touchstart' && e.targetTouches
-          ? e.targetTouches[0].pageY
-          : e.pageY || 0;
-      swipeState.startX = x;
-      swipeState.startY = y;
-      swipeState.isSwipe = true;
-      swipeState.swipeTime = Date.now();
+    if (swipeState.isSwipe === 'wait') {
+      const x = e.targetTouches ? e.targetTouches[0].pageX : e.pageX || 0;
+      const y = e.targetTouches ? e.targetTouches[0].pageY : e.pageY || 0;
+      swipeState.startSwipe(x, y);
     }
   };
   const handleMove = (e: Partial<TouchEvent & MouseEvent>) => {
-    if (swipeState.isSwipe && Container.current !== null) {
-      const x =
-        e.type === 'touchmove' && e.targetTouches
-          ? e.targetTouches[0].pageX
-          : e.pageX || 0;
-      const y =
-        e.type === 'touchmove' && e.targetTouches
-          ? e.targetTouches[0].pageY
-          : e.pageY || 0;
+    if (swipeState.isSwipe === 'pending' && Container.current !== null) {
+      const x = e.targetTouches ? e.targetTouches[0].pageX : e.pageX || 0;
+      const y = e.targetTouches ? e.targetTouches[0].pageY : e.pageY || 0;
       const offset = x - swipeState.startX - swipeState.currentX;
       if (Math.abs(swipeState.startY - y) < Math.abs(swipeState.startX - x)) {
         Container.current.style.transition = 'none';
@@ -52,15 +35,9 @@ export default function SwipeEvents(
     }
   };
   const handleEnd = (e: Partial<TouchEvent & MouseEvent>) => {
-    if (swipeState.isSwipe && Container.current !== null) {
-      const x =
-        e.type === 'touchend' && e.changedTouches
-          ? e.changedTouches[0].pageX
-          : e.pageX || 0;
-      const y =
-        e.type === 'touchend' && e.changedTouches
-          ? e.changedTouches[0].pageY
-          : e.pageY || 0;
+    if (swipeState.isSwipe === 'pending' && Container.current !== null) {
+      const x = e.changedTouches ? e.changedTouches[0].pageX : e.pageX || 0;
+      const y = e.changedTouches ? e.changedTouches[0].pageY : e.pageY || 0;
       const offset = swipeState.startX - x;
 
       if (
@@ -68,19 +45,17 @@ export default function SwipeEvents(
           Date.now() - swipeState.swipeTime < 200) &&
         Math.abs(swipeState.startY - y) < Math.abs(swipeState.startX - x)
       ) {
-        if (offset < 0 && swipeState.currentStep > 0) swipeState.currentStep--;
-        else if (offset > 0 && swipeState.currentStep < itemLength - 1)
-          swipeState.currentStep++;
+        offset < 0 ? swipeState.currentStep-- : swipeState.currentStep++;
       }
-      swipeState.isSwipe = null;
-      swipeState.swipeTime = 0;
-      swipeState.currentX =
+
+      swipeState.endSwipe(
         swipeState.currentStep *
-        parseFloat(getComputedStyle(Container.current).width);
+          parseFloat(getComputedStyle(Container.current).width),
+        333,
+      );
       Container.current.style.transition = '333ms';
       Container.current.style.transform = `translateX(-${swipeState.currentX}px)`;
       handleHistory();
-      setTimeout(() => (swipeState.isSwipe = false), 333);
     }
   };
   const handleResize = () => {
@@ -106,9 +81,7 @@ export default function SwipeEvents(
     if (config?.historyCallback) config.historyCallback(swipeState);
   };
   const handleSlide = (flag: 'L' | 'R') => {
-    if (flag === 'L' && swipeState.currentStep > 0) swipeState.currentStep--;
-    if (flag === 'R' && swipeState.currentStep < itemLength - 1)
-      swipeState.currentStep++;
+    flag === 'L' ? swipeState.currentStep-- : swipeState.currentStep++;
     handleHistory();
     handleInit();
   };
