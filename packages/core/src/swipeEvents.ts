3806a81a7ba8bb2476d91917d964532bda7b521a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-12-04 22:38:59 +0900

    refactor(CORE): SWIPE EVENTS - EARLY RETURN

--- packages/core/src/swipeEvents.ts
+++ packages/core/src/swipeEvents.ts
@@ -5,29 +5,27 @@ export const swipestart = (
   e: Partial<TouchEvent & MouseEvent>,
   swipeState: SwipeState,
 ) => {
-  if (swipeState.isSwipe === 'wait') {
-    const { x, y } = getStart(e);
-    swipeState.startSwipe(x, y);
-  }
+  if (swipeState.isSwipe !== 'wait') return;
+  const { x, y } = getStart(e);
+  swipeState.startSwipe(x, y);
 };
 export const swipeMove = (
   e: Partial<TouchEvent & MouseEvent>,
   swipeState: SwipeState,
   target: HTMLElement,
 ) => {
-  if (swipeState.isSwipe === 'pending') {
-    const { x, y, offset } = getMove(e, swipeState);
-    const shake =
+  if (swipeState.isSwipe !== 'pending') return;
+  const { x, y, offset } = getMove(e, swipeState);
+  const shake =
+    swipeState.direction === 'row'
+      ? Math.abs(swipeState.startY - y) < Math.abs(swipeState.startX - x)
+      : Math.abs(swipeState.startY - y) > Math.abs(swipeState.startX - x);
+  if (shake) {
+    target.style.transition = 'none';
+    target.style.transform =
       swipeState.direction === 'row'
-        ? Math.abs(swipeState.startY - y) < Math.abs(swipeState.startX - x)
-        : Math.abs(swipeState.startY - y) > Math.abs(swipeState.startX - x);
-    if (shake) {
-      target.style.transition = 'none';
-      target.style.transform =
-        swipeState.direction === 'row'
-          ? `translateX(${offset.x}px)`
-          : `translateY(${offset.y}px)`;
-    }
+        ? `translateX(${offset.x}px)`
+        : `translateY(${offset.y}px)`;
   }
 };
 export const swipeEnd = (
@@ -35,23 +33,22 @@ export const swipeEnd = (
   swipeState: SwipeState,
   target: HTMLElement,
 ) => {
-  if (swipeState.isSwipe === 'pending') {
-    const { x, y, offset } = getEnd(e, swipeState);
+  if (swipeState.isSwipe !== 'pending') return;
+  const { x, y, offset } = getEnd(e, swipeState);
+  swipeState.direction === 'row'
+    ? verticalSwipe(x, y, offset)
+    : horizontalSwipe(x, y, offset);
+  swipeState.endSwipe(
+    swipeState.currentStep * parseFloat(getComputedStyle(target).width),
+    swipeState.currentStep *
+      parseFloat(getComputedStyle(target.children[0]).height),
+    333,
+  );
+  target.style.transition = '333ms';
+  target.style.transform =
     swipeState.direction === 'row'
-      ? verticalSwipe(x, y, offset)
-      : horizontalSwipe(x, y, offset);
-    swipeState.endSwipe(
-      swipeState.currentStep * parseFloat(getComputedStyle(target).width),
-      swipeState.currentStep *
-        parseFloat(getComputedStyle(target.children[0]).height),
-      333,
-    );
-    target.style.transition = '333ms';
-    target.style.transform =
-      swipeState.direction === 'row'
-        ? `translateX(-${swipeState.currentX}px)`
-        : `translateY(-${swipeState.currentY}px)`;
-  }
+      ? `translateX(-${swipeState.currentX}px)`
+      : `translateY(-${swipeState.currentY}px)`;
 
   function verticalSwipe(
     x: number,
